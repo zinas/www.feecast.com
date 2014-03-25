3da89ecd062609e33ba48ddde11bbc98adc96563
zinas <zinas.nikos@gmail.com>
2014-03-25 21:36:30 +0100

    Content for about us and team pages

--- app/protected/views/site/pages/about.php
+++ app/protected/views/site/pages/about.php
@@ -1,1 +1,6 @@
-<h1>About us</h2>
+<h1>About us</h1>
+<p>At <strong>FeeCast</strong>, we are improving the way people make their decisions about healthcare by providing price transparency to health services, from doctor visits to surgeries.</p>
+
+<p><strong>For patients</strong> – using our online trusted source, we provide you with multiple health professionals nearby and their respective prices for your needs. You can compare and request an appointment with a health professional based on prices, ratings and reviews, accreditations, distance, and more. And best of all, FeeCast membership is free.</p>
+
+<p><strong>For health service professionals</strong> – we help providers and healthcare facilities, such as surgery centers and clinics, drive patients to you. With FeeCast, we connect you with qualified patients with real healthcare needs. Increase your patient flow and list your practice or facility today.</p>
--- app/protected/views/site/pages/team.php
+++ app/protected/views/site/pages/team.php
@@ -1,1 +1,132 @@
 <h1>Our team</h1>
+    <div class="well">
+        <center>
+        <a href="#aboutModal" data-toggle="modal" data-target="#myModal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/kristina.png" name="aboutme" width="140" height="140" class="img-circle"></a>
+        <h3>Kristina Serafim</h3>
+        <em>Co-Founder &amp; CEO</em>
+        </center>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title" id="myModalLabel">More About Kristina</h4>
+                    </div>
+                <div class="modal-body">
+                    <center>
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/kristina.png" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
+                    <h3 class="media-heading">Kristina Serafim</h3>
+                    <!-- <span><strong>Skills: </strong></span>
+                        <span class="label label-warning">HTML5/CSS</span>
+                        <span class="label label-info">Adobe CS 5.5</span>
+                        <span class="label label-info">Microsoft Office</span>
+                        <span class="label label-success">Windows XP, Vista, 7</span> -->
+                    </center>
+                    <hr>
+                    <center>
+                    <p class="text-left"><strong>Bio: </strong><br>
+                        Kristina is an experienced entrepreneur with more than fifteen years of experience. She previously founded two technology start-ups, coComment and JumpLane, from inception to acquisition. Kristina has also been on the investment side of the tech industry, and has a track record of launching Nielsen Gaming, iTV platform WatchNow, and Paramount’s international distribution arm. She holds an MBA from Harvard University and an electrical and mechanical engineering degree from Kettering University.</p>
+                    <br>
+                    </center>
+                </div>
+                <div class="modal-footer">
+                    <center>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">I've heard enough about Kristina</button>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="well">
+        <center>
+        <a href="#aboutModalGreg" data-toggle="modal" data-target="#gregModal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/greg.png" name="aboutme" width="140" height="140" class="img-circle"></a>
+        <h3>Greg Horner, M.D.</h3>
+        <em>Co-Founder &amp; President</em>
+        </center>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="gregModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title" id="myModalLabel">More About Greg</h4>
+                    </div>
+                <div class="modal-body">
+                    <center>
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/greg.png" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
+                    <h3 class="media-heading">Greg Horner <small>M.D.</small></h3>
+<!--                     <span><strong>Skills: </strong></span>
+                        <span class="label label-warning">HTML5/CSS</span>
+                        <span class="label label-info">Adobe CS 5.5</span>
+                        <span class="label label-info">Microsoft Office</span>
+                        <span class="label label-success">Windows XP, Vista, 7</span>
+ -->                    </center>
+                    <hr>
+                    <center>
+                    <p class="text-left">
+                        Greg Horner, M.D. is an accomplished Orthopedic Microsurgeon/Hand Surgeon, responsible for basic research discoveries leading to novel treatments for Orthopedic Surgery. In addition to leading a successful practice in California, Dr. Horner started several surgery centers across the nation. He serves on the Board of Directors of the Ambulatory Surgery Center Association and the California Ambulatory Surgery Association and is a leader in the American Society for Surgery of the Hand.
+<br><br>
+With more than fifteen years of experience, Dr. Horner deeply understands the challenges patients face when seeking health services. He is committed to changing and improving the way patients connect with health service providers through FeeCast.
+<br><br>
+Dr. Horner received his medical degree from Johns Hopkins Medical School and completed his residency at the UCLA Medical Center Department of Orthopedics. He also holds a B.S. from Northwestern University.</p>
+                    <br>
+                    </center>
+                </div>
+                <div class="modal-footer">
+                    <center>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">I've heard enough about Greg</button>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="well">
+        <center>
+        <a href="#aboutHippoModal" data-toggle="modal" data-target="#hippoModal"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ippokratis.png" name="aboutme" width="140" height="140" class="img-circle"></a>
+        <h3>Ippokratis Pandis</h3>
+        <em>Co-Founder &amp; CTO</em>
+        </center>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="hippoModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                    <h4 class="modal-title" id="myModalLabel">More About Ippokratis</h4>
+                    </div>
+                <div class="modal-body">
+                    <center>
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/ippokratis.png" name="aboutme" width="140" height="140" border="0" class="img-circle"></a>
+                    <h3 class="media-heading">Ippokratis Pandis</h3>
+                    <!-- <span><strong>Skills: </strong></span>
+                        <span class="label label-warning">HTML5/CSS</span>
+                        <span class="label label-info">Adobe CS 5.5</span>
+                        <span class="label label-info">Microsoft Office</span>
+                        <span class="label label-success">Windows XP, Vista, 7</span> -->
+                    </center>
+                    <hr>
+                    <center>
+                    <p class="text-left"><strong>Bio: </strong><br>
+                        Ippokratis is a technical expert on database technologies, including serving as a member of the research staff at IBM Almaden Research Center. His technical focus is on scalable hardware-aware data management. Ippokratis has been a core team member in the design of the IBM's DB2 BLU column-store database.
+<br><br>
+Ippokratis received his PhD from the Electrical and Computer Engineering department at Carnegie Mellon University. His many accomplishments include awards at several premier database systems conferences, such as ICDE, SIGMOD, CIDR, and others. He is currently serving as PC chair for DaMoN 2014 and PC area chair for the Main-memory, Parallel and Distributed Database systems area of CIKM 2014.</p>
+                    <br>
+                    </center>
+                </div>
+                <div class="modal-footer">
+                    <center>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">I've heard enough about Ippokratis</button>
+                    </center>
+                </div>
+            </div>
+        </div>
+    </div>
