36c69faf74ef58d76ea69e263cae3f10dd0f908a
Ippokratis Pandis <ippokratis@gmail.com>
2014-03-25 14:21:51 -0700

    Adding the (hopefully) correct DLL .sql

---
+++ app/protected/data/show-row-counts.sql
@@ -0,0 +1,5 @@
+select table_schema, count(table_schema) from information_schema.tables where table_schema = 'feecastdb' group by table_schema;
+select table_name, table_rows from information_schema.tables where table_schema = 'feecastdb';
+select table_schema, count(table_schema) from information_schema.tables where table_schema = 'kserafim_chi' group by table_schema;
+select table_name, table_rows from information_schema.tables where table_schema = 'kserafim_chi';
+select table_schema, count(table_schema) from information_schema.tables group by table_schema;
