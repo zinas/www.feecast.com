60c75080571ab03682d86b6608367b9007f12dae
zinas <zinas.nikos@gmail.com>
2014-03-29 20:54:12 +0100

    Put the dots in the home page bodies

--- app/protected/views/site/index.php
+++ app/protected/views/site/index.php
@@ -1,9 +1,34 @@
 <div class="jumbotron">
     <div class="container main-search-form">
 
-        <div class="">
+        <div class="body-map">
             <h3>Click an area of the body to see treatment prices</h3>
             <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/body.png" />
+
+            <div class="body-point body-point-1"></div>
+            <div class="body-point body-point-2"></div>
+            <div class="body-point body-point-3"></div>
+            <div class="body-point body-point-4"></div>
+            <div class="body-point body-point-5"></div>
+            <div class="body-point body-point-6"></div>
+            <div class="body-point body-point-7"></div>
+            <div class="body-point body-point-8"></div>
+            <div class="body-point body-point-9"></div>
+            <div class="body-point body-point-10"></div>
+            <div class="body-point body-point-11"></div>
+            <div class="body-point body-point-12"></div>
+            <div class="body-point body-point-13"></div>
+            <div class="body-point body-point-14"></div>
+            <div class="body-point body-point-15"></div>
+            <div class="body-point body-point-16"></div>
+            <div class="body-point body-point-17"></div>
+            <div class="body-point body-point-18"></div>
+            <div class="body-point body-point-19"></div>
+            <div class="body-point body-point-20"></div>
+            <div class="body-point body-point-21"></div>
+            <div class="body-point body-point-22"></div>
+            <div class="body-point body-point-23"></div>
+            <div class="body-point body-point-24"></div>
         </div>
 
         <div class="search-text">
