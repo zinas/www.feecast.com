d9f8eff35cb505a504c547ba76e337be9fdf0d19
Ippokratis <ippokratis@gmail.com>
2014-04-05 03:33:48 -0700

    Loading the CPTs, adding python, updating the dump

---
+++ app/protected/data/csv/load-db-cpt.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# Inserts the values of a csv file
+
+# For getting the script name and handling files
+import sys
+
+# For csv parsing
+import csv
+
+# For connecting to mysql
+import mysql.connector
+
+try:
+	cname = sys.argv[1];
+	print 'Reading: ' , cname
+except IndexError as e:
+	print 'Usage: ', sys.argv[0], " <csvfile>" 
+	print 'Need csv file as argument: %s' % (e)	
+	sys.exit(0)
+
+
+### Connect to MySQL
+try:
+	cnx = mysql.connector.connect(user='ip',password='diogenis',database='feecastdb')
+	cursor = cnx.cursor()
+except MySQLdb.Error, e:
+	print 'Connection error: ', e
+
+# Counting the number of entries 
+cnt = 1
+
+# Insert CPT statement
+insertCPTStmt = ("INSERT INTO cpt (id, cptCode, cptLongDesc, cptShortDesc, cptAddNotes) VALUES (%s, %s, %s, %s, %s)")
+
+# Open and read csv file
+with open(cname,"r") as datfile:
+	csvr = csv.reader(datfile, delimiter='|')
+	try:
+    		print "ID Code Long Short Note Body"
+       		for row in csvr:
+                    # CPT_LONG_DESCRIPTION|CPT_ADD_NOTE|CPT_CODE|CPT_SHORT_DESCRIPTION
+                    print cnt, '|', row[2], '|', row[0], '|', row[3], '|', row[1]
+                    dataCPT = (cnt,row[2],row[0],row[3],row[1])
+                    cursor.execute(insertCPTStmt,dataCPT)                    
+                    cnt = cnt + 1
+	except csv.Error as e:
+        	sys.exit('file %s, line %d: %s' % (rname, resultReader.line_num, e))
+
+print (cnt-1), ' plans added'
+
+# Make sure commits
+cnx.commit()
+cnx.close()
